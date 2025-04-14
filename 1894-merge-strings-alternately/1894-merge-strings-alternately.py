97edf8a6493b64d9db27a12ca4199e2c26867532
kiranmai1226 <104111067+kiranmai1226@users.noreply.github.com>
2025-04-14 11:43:42 +0530

    Time: 41 ms (37.78%), Space: 17.8 MB (33.8%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -3,14 +3,14 @@ class Solution:
         a=len(word1)
         b=len(word2)
         word3=""
-        min_index=min(a,b)
-        for i in range(min_index):
-            word3=word3+word1[i]+word2[i]
-        if a<b:    
-            return word3+word2[min_index:]
-        elif b<a:
-            return word3+word1[min_index:]
-        else:
-            return word3
+        i=0
+        j=0
+        while i<a and j<b:
+            word3=word3+word1[i]+word2[j]
+            i+=1
+            j+=1
+        word3=word3+word1[i:]+word2[j:]
+        return word3
+
 
         
