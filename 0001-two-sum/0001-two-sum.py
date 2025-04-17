a28b1f13397d00271215598d348e13d399aa56cc
kiranmai1226 <104111067+kiranmai1226@users.noreply.github.com>
2025-04-17 15:34:00 +0530

    Time: 0 ms (100%), Space: 18.9 MB (51.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,10 @@
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            difference=target-nums[i]
-            for j in range(i+1,len(nums)):
-                if difference==nums[j]:
-                    return [i,j]
-        
+        mydict={}
+        for i,num in enumerate(nums):
+            difference=target-num
+            if difference in mydict:
+                return [mydict[difference],i]
+            mydict[num]=i
+        return [] 
