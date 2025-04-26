07b5a6d7f3c4c6aa7e290fb40526fb438705f0eb
kiranmai1226 <104111067+kiranmai1226@users.noreply.github.com>
2025-04-26 11:02:03 +0530

    Time: 3 ms (66.42%), Space: 17.7 MB (86.68%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -1,24 +1,9 @@
 from collections import defaultdict
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        my_dict={chr(i):i-96 for i in range(97,123)}
-        result=0
-        result2=0
-        print(my_dict)
-        input=''
-        for i in s:
-            input+=str(my_dict[i])
-        input=int(input)
-        while k>0:
-            result=0
-            while input>0:
-                l=input%10
-                result=result+l
-                input=input//10
-            k-=1
-            input=result
-            
-        return result
-
+        nums=''.join(str(ord(i)-96) for i in s)
+        for _ in range(k):
+            nums=str(sum(int(num) for num in nums))
+        return int(nums)
 
       
