f71b1e158840a20d0e7d2a6a78929b5c8689ffe6
kiranmai1226 <104111067+kiranmai1226@users.noreply.github.com>
2026-05-17 09:57:36 +0530

    Time: 0 ms (100%), Space: 19.4 MB (12.26%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -2,11 +2,11 @@ from collections import defaultdict
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         count_1 = defaultdict(int)
-        out = []
+        out = set()
         for i in nums1:
             count_1[i] += 1
         for j in nums2:
-            if (j in count_1) and (j not in out):
-                out.append(j)
-        return out
+            if (j in count_1):
+                out.add(j)
+        return list(out)
         
