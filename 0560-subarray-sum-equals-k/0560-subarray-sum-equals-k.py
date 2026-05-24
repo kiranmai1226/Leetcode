be84255f718f88760ede8b9a87bfe6ef5884d7b8
kiranmai1226 <104111067+kiranmai1226@users.noreply.github.com>
2026-05-24 17:39:21 +0530

    Time: 47 ms (12.43%), Space: 23.2 MB (13.81%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -4,10 +4,8 @@ class Solution:
         count[0] = 1
         curr_sum = 0
         result = 0
-        for i in range(len(nums)):
-            curr_sum += nums[i]
+        for i in nums:
+            curr_sum += i
             result += count[curr_sum - k]
             count[curr_sum] += 1
         return result
-            
-        
