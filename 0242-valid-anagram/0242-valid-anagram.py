6ddac512db50711fab85924edda8a1d37c745e17
kiranmai1226 <104111067+kiranmai1226@users.noreply.github.com>
2026-05-24 12:20:51 +0530

    Time: 15 ms (43.16%), Space: 20.3 MB (15.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        arr = [0]*26
-        for i in range(len(s)):
-            if s[i] not in arr:
-                arr[ord(s[i]) - ord('a')] += 1
-            if t[i] not in arr:
-                arr[ord(t[i]) - ord('a')] -= 1
-        if not any(arr):
-            return True
-        else:
-            return False
+        s1 = sorted(s)
+        s2 = sorted(t)
+        return s1 == s2
